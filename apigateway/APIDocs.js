102629ec91a3d95f2f2753264adffad3cc25d0e1
pulcino <alfredo.pulcino@afp.com>
2018-09-27 17:04:33 +0200

    commit

---
+++ apigateway/APIDocs.js
@@ -0,0 +1,81 @@
+"use strict"
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+const config = require('config');
+const constantes = require('../library/constantes');
+const express = require('express');
+const router = express.Router();
+const consulMgr = require('../library/consulMgr').consulMgr;
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+let registryUrl = '/afpforum';
+if (config.has('Components.APIGateway.registry')) {
+    registryUrl = config.get('Components.APIGateway.registry');
+}
+
+//------------------------------------------------------------------------------
+// http://localhost:8080/api-docs
+// Utiliser l'url : http://127.0.0.1:8500/v1/agent/services
+//------------------------------------------------------------------------------
+router.get('/', (req, res) => {
+    consulMgr.GetAllHealthyServices(
+        (err) => {
+            res.writeHead(400, { 'Content-Type': 'application/json' });
+            res.write(err);
+            res.end();
+        },
+        (services) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(generateHTMLDocument(services));
+            res.end();
+        });
+})
+
+/**
+ * ------------------------------------------------------------------------------
+ * Générer une vue HTML avec les liens SWAGGER
+ *
+ * @param {Object[]} newList - Liste des composants disponibles [{"Address":"", "'Port":"", "url":""}].
+ * @returns {string} Page html contenant la liste des liens SWAGGER
+ * ------------------------------------------------------------------------------
+ */
+const generateHTMLDocument = function (newList) {
+    newList = newList || [];
+    let viewArr = [
+        '<!DOCTYPE html>',
+        '<html>',
+        '<head>',
+        '<title>Swagger links</title>',
+        '</head>',
+        '<body>',
+        generateHTMLView(newList),
+        '</body>',
+        '</html>'
+    ];
+    return viewArr.join('');
+}
+/**
+ * ------------------------------------------------------------------------------
+ * Générer une vue HTML avec les liens SWAGGER
+ *
+ * @param {Object[]} newList - Liste des composants disponibles.
+ * @returns {string} Page html contenant la liste des liens SWAGGER
+ * ------------------------------------------------------------------------------
+ */
+const generateHTMLView = function (newList) {
+    newList = newList || [];
+    let viewArr = [];
+    viewArr.push('<table id="linksTable">');
+    newList.forEach((value, index, array) => {
+        viewArr.push("<tr>");
+        viewArr.push('<td>' + value.name + ' : </td>');
+        viewArr.push('<td><a href="' + value.url + '/api-docs' + '">API description</a></td>');
+        viewArr.push('<td>' + value.url + '/api-docs' + '</td>');
+        viewArr.push("</tr>");
+    });
+    viewArr.push('</table>');
+    return viewArr.join('');
+}
+
+module.exports = router;
---
+++ apigateway/APIHealth.js
@@ -0,0 +1,23 @@
+"use strict"
+//------------------------------------------------------------------------------
+// 4.7.1. API de supervision
+// Il doit exposer en interne de la plate-forme les API REST suivantes :
+// /admin/v1/status : statut simple, renvoyant un statut de fonctionnement incluant des informations techniques sur l’état actuel du composant. Un exemple d’utilisation typique est l’intégration à un outil de supervision ou à un élément actif tiers (ex: load-balancer, ...) . L’appel doit être peu coûteux.
+// /admin/v1/autotest : autotest du composant, lançant un test de présence des différentes resources requises par le composant et renvoyant un statut d’état de ces resources.
+// /admin/v1/version : statut renvoyant les informations relatives à la version.
+//------------------------------------------------------------------------------
+const constantes = require('../library/constantes');
+const express = require('express');
+const router = express.Router();
+
+//------------------------------------------------------------------------------
+// http://localhost:8080/health/status
+//------------------------------------------------------------------------------
+router.get('/status', (req, res) => {
+    console.log("call for health/status")
+    res.status(200).json({
+        isSuccess: true
+    });
+})
+
+module.exports = router;
