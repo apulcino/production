d9b14f2c3cec607e773c2a2b4826715427ad32a9
pulcino <alfredo.pulcino@afp.com>
2018-10-22 17:09:16 +0200

    Creation images docker

--- linux-deploy.sh
+++ linux-deploy.sh
@@ -1,9 +1,21 @@
 cd apigateway
-sudo npm install
+npm install
 cd library
-sudo npm install
+npm install
 cd ..
+docker build -t afp/apigateway .
+docker run -p 8080:8080 -it afp/apigateway
 cd ..
+
+cd afoevents
+npm install
+cd library
+npm install
+cd ..
+docker build -t afp/afoevents .
+docker run -p 3100:3100 -it afp/afoevents
+cd ..
+
 
 
 
