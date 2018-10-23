437b33ac829a4f97d267ae67b4c156dff5fb8c0f
pulcino <alfredo.pulcino@afp.com>
2018-10-23 13:57:56 +0200

    pm2 deploy

--- pm2deploy.sh
+++ pm2deploy.sh
@@ -11,5 +11,12 @@ cd library
 npm install
 cd ..
 cd ..
+cd authent
+npm install
+cd library
+npm install
+cd ..
+cd ..
 pm2 startOrRestart ./apigateway/ecosystem.json --env production
 pm2 startOrRestart ./afoevents/ecosystem.json --env production
+pm2 startOrRestart ./authent/ecosystem.json --env production
