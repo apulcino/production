fdecfc60d0ad4caaf2242dfee8121dce904884a4
pulcino <alfredo.pulcino@afp.com>
2018-10-23 13:19:13 +0200

    pm2 deploy

--- pm2deploy.sh
+++ pm2deploy.sh
@@ -4,4 +4,11 @@ cd library
 npm install
 cd ..
 cd ..
+cd MSAFO/source/afoevents
+npm install
+cd library
+npm install
+cd ..
+cd ..
 pm2 startOrRestart ./apigateway/ecosystem.json --env production
+pm2 startOrRestart ./afoevents/ecosystem.json --env production
