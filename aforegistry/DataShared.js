858e2546602ca6458a1d306cb8c4e9d9d5a2723a
pulcino <alfredo.pulcino@afp.com>
2018-10-23 14:12:50 +0200

    reset

--- aforegistry/DataShared.js
+++
@@ -1,9 +0,0 @@
-const MServiceMgr = require('./MServiceMgr');
-exports.DataShared = {
-    traceMgr: null,
-    MServices: null,
-    initialize: function (traceMgr) {
-        this.traceMgr = traceMgr;
-        this.MServices = new MServiceMgr(this.traceMgr)
-    }
-}
