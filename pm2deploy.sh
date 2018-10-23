0a1078f2c39e682de1b3801edbe19ee6fb05e15d
pulcino <alfredo.pulcino@afp.com>
2018-10-23 14:20:11 +0200

    commit version : 2.25671620

--- pm2deploy.sh
+++ pm2deploy.sh
@@ -17,6 +17,13 @@ cd library
 npm install
 cd ..
 cd ..
+cd afopaniers
+npm install
+cd library
+npm install
+cd ..
+cd ..
 pm2 startOrRestart ./apigateway/ecosystem.json --env production
 pm2 startOrRestart ./afoevents/ecosystem.json --env production
 pm2 startOrRestart ./authent/ecosystem.json --env production
+pm2 startOrRestart ./afopaniers/ecosystem.json --env production
